deb7be96a2cfe5eae481f0915aae126bdecf44d3
Otokami-Orokabu <Otokami-Orokabu@users.noreply.github.com>
2025-03-27 15:46:51 +0900

    up漏れ

--- js/main.js
+++ js/main.js
@@ -637,5 +637,3 @@ function showShaderError(container, message) {
 function escapeHtml(html) {
     const div = document.createElement('div');
     div.textContent = html;
-    return div.innerHTML;
-}
